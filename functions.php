58d588b16c73d282602aefe51ec5ea1062e14872
HATSUMATSU <mail@hatsumatsu.de>
2014-07-10 23:22:54 +0200

    post type labels added

--- functions.php
+++ functions.php
@@ -110,7 +110,10 @@ register_post_type(
 	'projects', 
 	array( 'labels' => array( 
         'name' => __( 'Projects', 'hm_theme' ), 
-		'singular_name' => __( 'Project', 'hm_theme' ),
+        'singular_name' => __( 'Project', 'hm_theme' ),
+        'menu_name' => __( 'Project', 'hm_theme' ),
+        'menu_admin_bar' => __( 'Project', 'hm_theme' ),
+		'all_items' => __( 'All Projects', 'hm_theme' ),
 		'add_new_item' => __( 'Add new Project', 'hm_theme' ),
 		'edit_item' => __( 'Edit Project', 'hm_theme' ),
 		'new_item' => __( 'Add Project', 'hm_theme' ),
