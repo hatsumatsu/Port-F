d9a0e9d49208ec2882ce262ebdfcfff631fabf68
HATSUMATSU <mail@hatsumatsu.de>
2014-08-09 21:45:00 +0200

    added microformats to post time elements

--- inc/item-page.php
+++ inc/item-page.php
@@ -2,7 +2,7 @@
 
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-    <time class="posttime"><?php the_post_time(); ?></time>
+    <time class="posttime" datetime="<?php the_time( 'Y-m-d\TH:i' ); ?>"><?php the_post_time(); ?></time>
     
     <?php the_content(); ?>
 
