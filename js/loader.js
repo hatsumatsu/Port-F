f234ad8843d73bca98abde0b0aefeed26d6a147b
HATSUMATSU <mail@hatsumatsu.de>
2014-08-10 23:13:16 +0200

    fix indentation

--- js/loader.js
+++ js/loader.js
@@ -1,25 +1,25 @@
 jQuery( function( $ ) {
 
-  $( document ).ready( function() {
+    $( document ).ready( function() {
 
-      var min = ( globals.debug ) ? '' : '.min';
-      var src = ( globals.debug ) ? 'src/' : '';
+        var min = ( globals.debug ) ? '' : '.min';
+        var src = ( globals.debug ) ? 'src/' : '';
 
-      Modernizr.load( [
-        {
-        test: Modernizr.mq( globals.mediaQuery ),
-        yep : [ globals.blogurl + '/wp-content/themes/port-f/js/dependencies-smaller.min.js',
-                globals.blogurl + '/wp-content/themes/port-f/js/' + src + 'site-smaller' + min + '.js' ],
-        nope: [ globals.blogurl + '/wp-content/themes/port-f/js/dependencies-larger.min.js',
-                globals.blogurl + '/wp-content/themes/port-f/js/' + src + 'site-larger' + min + '.js' ]
-        },
-        { 
-        test: Modernizr.mq( 'only all' ),
-        // for IE lte 8 and browsers not supporting media queries
-        nope: []
-        } 
+        Modernizr.load( [
+            {
+                test: Modernizr.mq( globals.mediaQuery ),
+                yep : [ globals.blogurl + '/wp-content/themes/port-f/js/dependencies-smaller.min.js',
+                        globals.blogurl + '/wp-content/themes/port-f/js/' + src + 'site-smaller' + min + '.js' ],
+                nope: [ globals.blogurl + '/wp-content/themes/port-f/js/dependencies-larger.min.js',
+                        globals.blogurl + '/wp-content/themes/port-f/js/' + src + 'site-larger' + min + '.js' ]
+            },
+            { 
+                test: Modernizr.mq( 'only all' ),
+                // for IE lte 8 and browsers not supporting media queries
+                nope: []
+            } 
         ] );
 
-  });
+  } );
 
-});
+} );
