b58d78421af8242374df02df819c38079c1b30f4
HATSUMATSU <mail@hatsumatsu.de>
2014-09-13 18:04:10 +0200

    improved search results

--- search.php
+++ search.php
@@ -4,23 +4,22 @@
 
 global $wp_query;
 $count = $wp_query->post_count;
+$key = get_search_query(); 
 
-if( $count > 0 ) { 
-    $message = sprintf( __( 'There are %1$s search results for ', 'hm_theme' ), $count );
-} elseif( $count == 1 ) {
-    $message = __( 'There is one search result for ', 'hm_theme' );
+if( $count == 1 ) { 
+    $message = sprintf( __( 'There is one search result for <em>%1$s.</em>', 'hm_theme' ), esc_html( $key ) );
+} elseif( $count > 1 ) {
+    $message = sprintf( __( 'There are %1$s search results for <em>%2$s.</em>', 'hm_theme' ), esc_html( $count ), esc_html( $key ) );
 } else { 
-    $message = __( 'Sorry, we couldn&apos;t find anything that matches ', 'hm_theme' );
+    $message = sprintf( __( 'Sorry, we couldn&apos;t find anything that matches <em>%1$s.</em>', 'hm_theme' ), esc_html( $key ) );
 }
-
-$key = wp_specialchars( $s, 1 ); 
 
 get_search_form(); 
 
 ?>
 
 <div class="message">
-    <?php echo esc_html( $message ); ?>
+    <?php echo $message; ?>
 </div>
 
 <?php if( have_posts() ) { ?>  
@@ -43,7 +42,7 @@ foreach( $post_types as $post_type ) {
     $query = new WP_Query( 
         array(
             'post_type' => $post_type,
-            'showposts' => -1,
+            'posts_per_page' => -1,
             's' => $key
         ) 
     );
@@ -53,7 +52,7 @@ foreach( $post_types as $post_type ) {
     if( $query->post_count > 0 ) { ?>  
 
         <section class="posts posts-<?php echo $post_type; ?>">
-            <h4><?php echo esc_html( $post_type_object->labels->name;  ); ?></h4>
+            <h4><?php echo esc_html( $post_type_object->labels->name ); ?></h4>
 
         <?php while( $query->have_posts() ) { $query->the_post(); ?>  
       
