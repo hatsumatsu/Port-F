38bd30a91094fcb2c22ae691e5035f4db93a08a6
HATSUMATSU <mail@hatsumatsu.de>
2014-11-10 18:01:31 +0100

    404 cleanup

--- 404.php
+++ 404.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
-<h2><?php _e( 'Ouuups...' ); ?></h2>
+<h2><?php echo __( 'Ouuups...', 'hm_theme' ); ?></h2>
 
-<p class="message">
-    <?php _e( 'This page doesn&rsquo;t exist.' ); ?>
+<p>
+    <?php echo __( 'This page doesn&apos;t exist.', 'hm_theme' ); ?>
 </p>
 
 <?php get_footer(); ?>
