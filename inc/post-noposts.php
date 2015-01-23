f80057078414215a4d031f7a48b1c5b4df4b585a
HATSUMATSU <mail@hatsumatsu.de>
2015-01-23 17:46:41 +0100

    add handling of no post scenario

---
+++ inc/post-noposts.php
@@ -0,0 +1,5 @@
+<article class="post post-noposts">
+
+    <h2><?php echo __( 'There are no posts in this section', 'hm_theme' ); ?></h2>
+    
+</article>
--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 ﻿<?php get_header(); ?>
 
-<?php if( have_posts() ) { ?>
-
 <section class="posts posts-<?php echo get_post_type_advanced(); ?>"
 	data-url="<?php echo esc_attr( get_current_archive_url() ); ?>"
 	data-page="<?php echo esc_attr( get_query_var( 'paged' ) ); ?>" 
 	data-pages-total="<?php echo esc_attr( $wp_query->max_num_pages ); ?>">
+
+<?php if( have_posts() ) { ?>
 
     <?php while( have_posts() ) { the_post(); ?>
 
@@ -13,10 +13,14 @@
 
     <?php } /* endwhile */ ?>
 
-</section>
-
 <?php get_inc( 'pagination', 0, 0 ); ?>
 
-<?php } /* endif */ ?>
+<?php } else { ?>
+
+    <?php get_inc( 'post', 'noposts', true ); ?>
+
+<?php } ?>
+
+</section>
 
 <?php get_footer(); ?>
