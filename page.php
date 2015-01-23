838a1d3c455efc5066d4ba79e502280df24f5432
HATSUMATSU <mail@hatsumatsu.de>
2015-01-23 17:43:20 +0100

    fix inconsistent code style

--- page.php
+++ page.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) { ?>
+<?php if( have_posts() ) { ?>
 
-<?php while ( have_posts() ) { the_post(); ?>
+<?php while( have_posts() ) { the_post(); ?>
 
+	<h2><?php the_title(); ?></h2>
 	<?php the_content(); ?>
 
 <?php } /* endwhile */ ?>
