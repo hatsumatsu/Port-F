75d9819389378ddfb2deef99ed840b4373fdfa7f
HATSUMATSU <mail@hatsumatsu.de>
2015-09-15 08:37:53 +0200

    formatted pagination template

--- inc/pagination.php
+++ inc/pagination.php
@@ -1,25 +1,28 @@
 <?php 
 global $wp_rewrite;     
     
-$current = ( $wp_query->query_vars[ 'paged' ] > 1 ) ? $wp_query->query_vars[ 'paged' ] : 1; 
-
+$current = ( $wp_query->query_vars['paged'] > 1 ) ? $wp_query->query_vars['paged'] : 1;
 $pagination = array(
-    'base' => @add_query_arg( 'page', '%#%' ),
-    'format' => '',
+    'base'      => @add_query_arg( 'page', '%#%' ),
+    'format'    => '',
     'prev_text' => '&lsaquo;',
     'next_text' => '&rsaquo;',
-    'total' => $wp_query->max_num_pages,
-    'current' => $current,
-    'show_all' => false,
-    'type' => 'plain',
-    );
+    'total'     => $wp_query->max_num_pages,
+    'current'   => $current,
+    'show_all'  => false,
+    'type'      => 'plain'
+);
 
 if( $wp_rewrite->using_permalinks() ) {
-    $pagination[ 'base' ] = user_trailingslashit( trailingslashit( remove_query_arg( 's', get_pagenum_link( 1 ) ) ) . 'page/%#%/', 'paged' );
+    $pagination['base'] = user_trailingslashit( trailingslashit( remove_query_arg( 's', get_pagenum_link( 1 ) ) ) . 'page/%#%/', 'paged' );
 }
 
-if( !empty( $wp_query->query_vars[ 's' ] ) ) {
-    $pagination[ 'add_args' ] = array( 's' => get_query_var( 's' ) );
+if( !empty( $wp_query->query_vars['s'] ) ) {
+    $pagination['add_args'] = array( 's' => get_query_var( 's' ) );
 }
 
-echo '<nav class="pagination">' . paginate_links( $pagination ) . '</nav>';
+?>
+
+<nav class="pagination">
+    <?php echo paginate_links( $pagination ); ?>
+</nav>
