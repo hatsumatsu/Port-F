d320271fd02baed2c2a67549cb47ebaef60bf70c
HATSUMATSU <mail@hatsumatsu.de>
2018-06-19 17:56:12 +0200

    fixed debug switch

--- js/src/app/debug.js
+++ js/src/app/debug.js
@@ -1,66 +1,22 @@
 /**
  * Debug
  */
-Debug = ( function() {
-
-    var settings = {
-        keyboard: {
-            key: 68, // 'D'
-            count: 0,
-            countMax: 7,
-            timeDuration: 5000,
-            timer: null
-        },
-    }
-
-    var state = {
+Debug = {
+    state: {
         local: false,
         active: true
-    }
+    },
 
-    var init = function() {
-        state.local = ( location.href.indexOf( 'local' ) > -1 ) ? true : false;
-        state.active = state.local;
-        // state.active = false;
+    init: function() {
+        this.state.local = ( location.href.indexOf( 'local' ) > -1 ) ? true : false;
+        this.state.active = this.state.local;
 
-        bindEventHandlers();
-    }
-
-    var bindEventHandlers = function() {
-        document
-            .addEventListener( 'keyup', function( event ) {
-                if( event.which == settings.keyboard.key ) {
-                    log( 'hit the toggle key' );
-
-                    if( settings.keyboard.timer ) {
-                        clearTimeout( settings.keyboard.timer );
-                    }
-
-                    settings.keyboard.timer = setTimeout( function() {
-                        settings.keyboard.count = 0;
-                        clearTimeout( settings.keyboard.timer );
-                    }, settings.keyboard.timeDuration );
-
-                    settings.keyboard.count = settings.keyboard.count + 1;
-                    if( settings.keyboard.count == settings.keyboard.countMax ) {
-                        state.active = !state.active;
-                        log( 'toggle logging', state.active );
-                    }
-                }
-            } );
-    }
-
-    var log = function() {
-        if( state.active ) {
-            return Function.prototype.bind.call( console.log, console, 'Port-F:' );
+        if( this.state.active ) {
+            this.log = console.log.bind( window.console );
+        } else {
+            this.log = function() {}
         }
-    }();
-
-    return {
-        init: function() { init(); },
-        log:  log
     }
-} )();
-
+}
 
 Debug.init();
