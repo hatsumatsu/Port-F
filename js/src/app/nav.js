75334b49a259c833ec9cc8b6de20cdf37ce547b7
Martin Wecke <mail@hatsumatsu.de>
2018-11-15 09:39:18 +0100

    updated nav.js to match module boilerplate

--- js/src/app/nav.js
+++ js/src/app/nav.js
@@ -4,20 +4,31 @@
 jQuery( function( $ ) {
 
     Nav = ( function() {
+        var namespace = '.nav';
 
-        var settings = {}
+        var settings = {
+            mediaQuery: 'screen',
+        }
 
         var selector = {
             nav: '[data-nav-role="nav"]',
-            toggle: '[data-nav-role="toggle"]'
+            toggle: '[data-nav-role="toggle"]'            
         }
 
-        var state = {}
+        var state = {
+            initiated: false
+        }
 
-        var init = function() {
-            Debug.log( 'Nav.init()' );
+        var setup = function() {
+            Debug.log( 'Module.setup()' );
+
+            if( state.initiated ) {
+                return false;
+            }
 
             bindEvents();
+
+            state.initiated = true;
         }
 
         var bindEvents = function() {
@@ -27,30 +38,82 @@ jQuery( function( $ ) {
 
                     var id = $( this ).attr( 'data-nav-id' );
 
-                    if( id ) {
-                        if( $( 'html' ).hasClass( 'visible--nav-' + id  ) ) {
-                            $( document ).trigger( 'nav/hide', [{
-                                id: id
-                            }] );
-                        } else {
-                            $( document ).trigger( 'nav/show', [{
-                                id: id
-                            }] );
-                        }
-
-                        $( 'html' ).toggleClass( 'visible--nav-' + id );
-                    }
+                    toggle( id );
                 } );
         }
 
-        return {
-            init: function() { init(); }
+        var toggle = function( id ) {
+            Debug.log( 'Nav.toggle()', id );
+
+            if( !id ) { 
+                return false;
+            }
+
+            if( $( 'html' ).hasClass( 'visible--nav-' + id  ) ) {
+                $( document ).trigger( 'nav/hide', [ {
+                    id: id
+                } ] );
+            } else {
+                $( document ).trigger( 'nav/show', [ {
+                    id: id
+                } ] );
+            }
+
+            $( 'html' ).toggleClass( 'visible--nav-' + id );
         }
 
+
+        /**
+         * Factory functions
+         */
+        var _init = function() {
+            Debug.log( 'Nav._init()' );
+
+            // listen for resize event
+            $( document )
+                .on( 'viewport/resize/finish', function() {
+                    _onResize();
+                } )
+
+            _checkMediaQuery();
+        }
+
+        var _onResize = function() {
+            _checkMediaQuery();
+        }
+
+        var _setup = function() {
+            setup();
+        }
+
+        var _destroy = function() {
+            if( !state.initiated ) {
+                return false;
+            }
+
+            $( document ).off( namespace )
+            state.initiated = false;
+
+            if( typeof destroy === 'function' ) {
+                destroy();
+            }
+        }
+
+        var _checkMediaQuery = function() {
+            if( window.matchMedia( settings.mediaQuery ).matches ) {
+                _setup();
+            } else {
+                _destroy();
+            }
+        }
+
+        return {
+            init: function() { _init(); }
+        }
     } )();
 
-    $( document ).ready( function () {
+
+    $( document ).ready( function() {
         Nav.init();
     } );
-
 } );
