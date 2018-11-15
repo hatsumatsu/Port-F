9a6652aca2bccbcee0acb37f02338fad3137ce73
Martin Wecke <mail@hatsumatsu.de>
2018-11-15 09:00:42 +0100

    Redirect 404 to home

--- 404.php
+++ 404.php
@@ -1,9 +1,3 @@
-<?php get_header(); ?>
-
-<h2><?php echo __( 'Sorry', 'port-f' ); ?></h2>
-
-<p>
-    <?php echo __( 'This page doesn&apos;t exist.', 'port-f' ); ?>
-</p>
-
-<?php get_footer(); ?>
+<?php
+    wp_redirect( get_site_url( 0, '/' ), 302 );
+    exit;
