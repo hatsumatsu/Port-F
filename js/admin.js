dec36d2b3378a96679e57742109eada8af0a93a5
HATSUMATSU <mail@hatsumatsu.de>
2019-02-06 09:46:50 +0100

    fixed JS error in admin

--- js/admin.js
+++ js/admin.js
@@ -1,11 +1,11 @@
+console.log( 'admin.js' );
+
 /**
  * Fix a bug where images edited in the WP Editor are altered in an undocumented way
  * and don't respect the
  * https://core.trac.wordpress.org/ticket/34823
  */
-console.log( 'admin.js' );
-
-if ( wp.media.events ) {
+if( wp && wp.media && wp.media.events ) {
     wp.media.events.on( 'editor:image-update', function( data ) {
         console.log( 'editor:image-update' );
         if( !data.editor.$( data.image ).attr( 'srcset' ) && data.editor.$( data.image ).attr( 'data-srcset' ) ) {
