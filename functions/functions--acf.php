f9a713dfbed974dbc5b22b251f5013117ff57929
Martin Wecke <>
2019-06-21 10:12:53 +0200

    added ACF filter to prevent field changes in live sites

--- functions/functions--acf.php
+++ functions/functions--acf.php
@@ -18,3 +18,19 @@ function modifyACFEditorToolbars( $toolbars ) {
 }
 
 add_filter( 'acf/fields/wysiwyg/toolbars' , 'modifyACFEditorToolbars'  );
+
+
+
+/**
+ * Prevent overwriting ACF fields when doing field changes in the live site
+ * Useful when on multisite and sharing the same theme
+ * so changes in one site don't affect other sites...
+ */
+add_filter( 'acf/settings/save_json', function( $path ) {
+    if( strpos( get_bloginfo( 'url' ), 'local' ) !== false ) {
+        $path = $path . '/live';
+    }
+
+    return $path;
+} );
+
