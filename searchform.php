93d0af644076c622f6230f05f6da907b7adf28e6
Martin Wecke <>
2019-09-04 15:33:06 +0200

    clean up search form markup

--- searchform.php
+++ searchform.php
@@ -1,4 +1,20 @@
-<form action="<?php echo esc_attr( get_bloginfo( 'wpurl' ) ); ?>" method="get" class="searchform" role="search">
-    <input type="text" class="searchform-text" name="s" id="search" value="<?php the_search_query(); ?>" placeholder="<?php echo esc_attr( __( 'Enter your search term...', 'port-f' ) ); ?>">
-    <input type="submit" class="searchform-submit" value="<?php _e( 'Search' ); ?>">
+<form
+    action="<?php echo esc_attr( get_bloginfo( 'wpurl' ) ); ?>"
+    method="get"
+    class="searchform"
+    role="search">
+
+    <input
+        type="text"
+        class="searchform-text"
+        name="s"
+        id="search"
+        value="<?php the_search_query(); ?>"
+        placeholder="<?php echo esc_attr( __( 'Enter your search term...', 'port-f' ) ); ?>">
+
+    <input
+        type="submit"
+        class="searchform-submit"
+        value="<?php echo __( 'Search', 'port-f' ); ?>">
+
 </form>
