a22f9776e29ea776330d83976b2d3eacce813bca
Martin Wecke <>
2020-04-28 19:45:21 +0200

    prefix W3TC minified file names

--- functions/functions--w3tc.php
+++ functions/functions--w3tc.php
@@ -22,3 +22,18 @@ function W3TCflushCustomPostTypesPageCache( $post_id ) {
 add_action( 'save_post', 'W3TCflushCustomPostTypesPageCache', 1000, 1 );
 add_action( 'edit_post', 'W3TCflushCustomPostTypesPageCache', 1000, 1 );
 add_action( 'delete_post', 'W3TCflushCustomPostTypesPageCache', 1000, 1 );
+
+
+
+/**
+ * Prefix minified file names with theme evrsion to bust cache more aggressively
+ *
+ */
+function W3TCModifyMinifyFileNames( $filename, $files, $type ) {
+    $prefix = sanitize_title( str_replace( '.','', wp_get_theme()->version ) );
+    $filename = $prefix .'_' . $filename;
+
+    return $filename;
+}
+
+add_filter( 'w3tc_minify_urls_for_minification_to_minify_filename', 'W3TCModifyMinifyFileNames', 20, 3 );
