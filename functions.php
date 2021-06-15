24c7489a5fd448f2b5e9c99bf5ada5eae1cd8a23
Martin Wecke <mail@hatsumatsu.de>
2021-06-15 15:56:36 +0200

    renaming files

--- functions.php
+++ functions.php
@@ -1,17 +1,17 @@
 <?php
 
-require_once( 'functions/functions--assets.php' );
-require_once( 'functions/functions--data.php' );
-require_once( 'functions/functions--nav.php' );
-require_once( 'functions/functions--admin.php' );
-require_once( 'functions/functions--editor.php' );
-require_once( 'functions/functions--search.php' );
-require_once( 'functions/functions--images.php' );
-require_once( 'functions/functions--template-tags.php' );
-require_once( 'functions/functions--theme.php' );
-require_once( 'functions/functions--permalink.php' );
-require_once( 'functions/functions--query.php' );
+require_once( 'functions/assets.php' );
+require_once( 'functions/data.php' );
+require_once( 'functions/nav.php' );
+require_once( 'functions/admin.php' );
+require_once( 'functions/editor.php' );
+require_once( 'functions/search.php' );
+require_once( 'functions/images.php' );
+require_once( 'functions/template-tags.php' );
+require_once( 'functions/theme.php' );
+require_once( 'functions/permalink.php' );
+require_once( 'functions/query.php' );
 
 // plugin related functions
-require_once( 'functions/functions--acf.php' );
-require_once( 'functions/functions--w3tc.php' );
+require_once( 'functions/acf.php' );
+require_once( 'functions/w3tc.php' );
