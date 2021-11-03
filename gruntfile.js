ac17e8312499541b435691e903598b378b8d1985
Martin Wecke <mail@hatsumatsu.de>
2021-11-03 21:17:38 +0100

    change postcss parser

--- gruntfile.js
+++ gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function (grunt) {
                 parser: require('postcss-scss'),
 
                 processors: [
-                    require('postcss-partial-import')(),
+                    require('postcss-import')(),
                     require('postcss-custom-media')(),
                     require('postcss-mixins')(),
                     require('postcss-nested')(),
