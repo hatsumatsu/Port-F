c1c784dbd2f09133cc0cb5dfac79d9678dcb939d
Martin Wecke <mail@hatsumatsu.de>
2021-11-03 17:40:33 +0100

    WIP updating dependencies

--- gruntfile.js
+++ gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
 
         postcss: {
             options: {
-                parser: require('postcss-comment'),
+                parser: require('postcss-scss'),
 
                 processors: [
                     require('postcss-partial-import')(),
