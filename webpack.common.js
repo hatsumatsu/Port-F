8b36f33c364b7ed5b351e2103666747ccf652e44
Martin Wecke <mail@hatsumatsu.de>
2021-11-04 16:59:20 +0100

    don't transpile JS to es5 during development

--- webpack.common.js
+++ webpack.common.js
@@ -6,19 +6,4 @@ module.exports = {
     output: {
         sourceMapFilename: '../js/sourcemaps/[file].map',
     },
-
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'node_modules/@superstructure.net')],
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                },
-            },
-        ],
-    },
 };
