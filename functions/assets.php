481ac6c59b0287fef56433b1dde7ed1fb6019067
Martin Wecke <mail@hatsumatsu.de>
2024-12-18 10:49:55 +0100

    Remove more core assets

--- functions/assets.php
+++ functions/assets.php
@@ -6,6 +6,9 @@
 function assetsRegisterThemeCSS() {
     // - block library styles
     wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
+    wp_dequeue_style( 'global-styles' );
+    wp_dequeue_style( 'classic-theme-styles' );
 
     // + theme
     wp_register_style(
